ccb0b203a6a581051e0a3cec8e7d3e9a2eb138df
Klaudia Brzywcy <kbrzywcy@gmail.com>
2022-05-10 15:16:57 +0200

    styles for MovieCard component added

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import './MovieCard.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import {faImdb} from "@fortawesome/free-brands-svg-icons"
 import axios from "axios";
 
 const key = 'a82d2d62';
@@ -43,27 +44,35 @@ const MovieCard = ({result, closeMovieCard}) => {
     return (
         <section className="movie-card" >
             <div className="content">
+                <div className="movie-card-text-container">
                 <h2>{Title}</h2>
-                {/* <h3>{result.imdbID}</h3> */}
-                <div>
-                <h3>Rating on IMDb:{imdbRating}</h3>
-                <FontAwesomeIcon icon={faStar} />
-                </div>
+               
+                <div className="rating-container">
+                <span className="imdb-icon-span"><FontAwesomeIcon icon={faImdb} /></span>
+                 <span className="number-rate">  rating:  {imdbRating}</span>
+
+                </div> 
                 
-                <h3>Released: {Released}</h3>
-                <div className="movie-card-poster-container">
-                <img src={Poster} alt="Poster of selected movie" />
-                </div>
+                <h4>Released: {Released}</h4>
                 <p>{Plot}</p>
-                <div>{Genre &&
+                <div className="genre-div">{Genre &&
                             Genre.split(', ').map(g => (
                                 <span className="genre-span" key={g}>{g}</span>
-                            ))}</div>
+                         ))
+                    }
+                </div>
 
-                
-                
-                
                 <button className="close-card" onClick={() => closeMovieCard(result.imdbID)} >Close</button>
+
+                </div>
+               
+                <div className="movie-card-poster-container">
+                    
+                    <img src={Poster} alt="Poster of selected movie" />
+                
+                </div>
+                
+               
             </div>
         </section>
     )
@@ -71,17 +80,3 @@ const MovieCard = ({result, closeMovieCard}) => {
 
 export default MovieCard;
 
-// axios.get(URL + '&i='+ `${result.imdbID}`)
-//         .then(({data}) => {
-//             //  console.log(data.Title) 
-             
-//              console.log(data)
-            
-//             setState(prevState => {
-                
-//                 return (
-//                   {...prevState, movieResults : data, isOpen: true})
-//               })
-
-              
-//         })
--- src/components/MovieCardClass.js
+++ src/components/MovieCardClass.js
@@ -1,53 +1,53 @@
-import axios from "axios";
-import React from "react";
-import ContainerClass from "./ContainerClass";
+// import axios from "axios";
+// import React from "react";
+// import ContainerClass from "./ContainerClass";
 
-const key = 'a82d2d62';
+// const key = 'a82d2d62';
  
-class MovieCard extends React.Component {
-    constructor (props) {
+// class MovieCard extends React.Component {
+//     constructor (props) {
 
-        super (props);
+//         super (props);
 
-        this.state = {
+//         this.state = {
 
-            movieData : {}
-        };
+//             movieData : {}
+//         };
 
-    }
+//     }
 
-    componentDidMount(){
-        axios.get(
-            `https://www.omdbapi.com/?apikey=${key}&i=${
-                this.props.movieID
-            }&plot=full`
-        )
-        .then(res => res.data)
-        .then(res => {
-            this.setState({movieData : res});
-            console.log(res)
+//     componentDidMount(){
+//         axios.get(
+//             `https://www.omdbapi.com/?apikey=${key}&i=${
+//                 this.props.movieID
+//             }&plot=full`
+//         )
+//         .then(res => res.data)
+//         .then(res => {
+//             this.setState({movieData : res});
+//             console.log(res)
             
-        })
-    }
+//         })
+//     }
 
-    render() {
+//     render() {
 
-        const {
-            Title,
-            Released,
-            Genre,
-            Plot,
-            Poster,
-            imdbrating
-        } = this.state.movieData
+//         const {
+//             Title,
+//             Released,
+//             Genre,
+//             Plot,
+//             Poster,
+//             imdbrating
+//         } = this.state.movieData
 
-        return (
-            <div className="movie-container">
-                <img src={Poster} alt="" />
-            </div>
+//         return (
+//             <div className="movie-container">
+//                 <img src={Poster} alt="" />
+//             </div>
 
-        )
-    }
-}
+//         )
+//     }
+// }
 
-export default MovieCard;
+// export default MovieCard;
