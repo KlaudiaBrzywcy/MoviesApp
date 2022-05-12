5ac28b206cb490b59658dccd1d000c2cb0fc50e1
mike-czarnota <czarnota.michael94+github@gmail.com>
2022-05-12 18:38:11 +0200

    Fix Nav Item

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { NavLink as RouterNavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const NavItem = ({ path, icon, name }) => {
   return (
     <li className='nav-item'>
-      <a href={path} tag={RouterNavLink} exact to={path} >
+      <Link to={path} >
         <FontAwesomeIcon icon={icon} />
         {/* {' '} */}
         {name}
-      </a>
+      </Link>
     </li>
   )
 };
